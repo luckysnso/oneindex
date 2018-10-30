28793ecc7af6240b4417c02dd03b39776eaa8204
luckysnso <44566083+luckysnso@users.noreply.github.com>
2018-10-30 19:36:57 +0800

    Update AdminController.php
    更新调用ju.tn中转使用获取本地连接

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -153,10 +153,11 @@ class AdminController{
 		if($_SERVER['HTTP_HOST'] == 'localhost'){
 			$redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].get_absolute_path(dirname($_SERVER['PHP_SELF']));
 		}
-		//else{
+		else{
 			// 非https,调用ju.tn中转
-		//	$redirect_uri = 'https://ju.tn/';
-		//}
+			//$redirect_uri = 'https://ju.tn/';
+			$redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].get_absolute_path(dirname($_SERVER['PHP_SELF']));
+		}
 		
 		$ru = "https://developer.microsoft.com/en-us/graph/quick-start?appID=_appId_&appName=_appName_&redirectUrl={$redirect_uri}&platform=option-php";
 		$deepLink = "/quickstart/graphIO?publicClientSupport=false&appName=oneindex&redirectUrl={$redirect_uri}&allowImplicitFlow=false&ru=".urlencode($ru);
